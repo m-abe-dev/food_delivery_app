f5c5f66272b209425874fdc082a6e035a4dd90b8
masashiabe <tech.abe.jp@gmail.com>
2021-07-01 01:36:29 +0900

    コンポーネントからAPIを叩く

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants = () => {
+
+  // 初回レンダリングの一度だけ
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+  
   return (
     <Fragment>
       レストラン一覧
